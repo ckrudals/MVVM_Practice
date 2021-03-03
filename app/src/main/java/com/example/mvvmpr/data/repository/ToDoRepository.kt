34ebb43469d627b350239cb037f2e3073b69c8ef
KyungMin <2004ckck@naver.com>
2021-03-03 20:25:24 +0900

    [CREATED] Database

--- app/src/main/java/com/example/mvvmpr/data/repository/ToDoRepository.kt
+++ app/src/main/java/com/example/mvvmpr/data/repository/ToDoRepository.kt
@@ -4,12 +4,15 @@ import androidx.lifecycle.LiveData
 import com.example.mvvmpr.data.ToDoDao
 import com.example.mvvmpr.data.models.ToDoData
 
+// repository 를 쓰는 이유
+// 모듈화가 잘되 분담하기가 쉽다
+// 현업에서 MVVM 패턴을 쓰는 이유기도 하다, 하지만 단점으로는
+// 너무 많은 Class 를 생성해야 하기 때문에 보기 불편 하다.
 class ToDoRepository(private val toDoDao: ToDoDao) {
     val getAllData: LiveData<List<ToDoData>> = toDoDao.getAllData()
 
     suspend fun insertData(toDoData: ToDoData) {
        toDoDao.insertData(toDoData)
-
 
     }
 }
