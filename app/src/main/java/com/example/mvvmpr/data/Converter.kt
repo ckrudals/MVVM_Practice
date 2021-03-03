31bc549e6c757db7b48df81c216e2db1f2cb4513
KyungMin <2004ckck@naver.com>
2021-03-03 20:23:59 +0900

    [CREATED] TypeConverter

--- app/src/main/java/com/example/mvvmpr/data/Converter.kt
+++ app/src/main/java/com/example/mvvmpr/data/Converter.kt
@@ -5,10 +5,12 @@ import com.example.mvvmpr.data.models.Priority
 
 class Converter {
 
+    //Priority.HIGH 를 toString() 으로 가능하게 해준다.
     @TypeConverter
     fun fromPriority(priority: Priority): String{
         return priority.name
     }
+    @TypeConverter
     fun  toPriority(priority:String): Priority {
         return Priority.valueOf(priority)
     }
