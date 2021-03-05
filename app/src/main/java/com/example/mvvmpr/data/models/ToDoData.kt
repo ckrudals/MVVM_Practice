b567421f1bcc0a008751328ab1faca8ac690ed78
KyungMin <2004ckck@naver.com>
2021-03-06 00:53:39 +0900

    [Create] updateData,deleteItem,deleteAll

--- app/src/main/java/com/example/mvvmpr/data/models/ToDoData.kt
+++ app/src/main/java/com/example/mvvmpr/data/models/ToDoData.kt
@@ -1,14 +1,18 @@
 package com.example.mvvmpr.data.models
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.mvvmpr.data.models.Priority
+import kotlinx.android.parcel.Parcelize
 
+@Suppress("DEPRECATED_ANNOTATION")
 @Entity(tableName="todo_table")
+@Parcelize
 data class ToDoData(
         @PrimaryKey(autoGenerate=true) //자동 생성
         var id: Int,
         var title: String,
         var priority: Priority,
         var description: String
-)
+) : Parcelable
